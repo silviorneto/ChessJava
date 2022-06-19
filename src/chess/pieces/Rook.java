567a31601badbbc1e1687bc3855db199dd40cbdb
silvio <silvioromano.dev@gmail.com>
2022-06-19 18:16:47 -0300

    Implementando lógica de xeque mate

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -65,9 +65,7 @@ public class Rook extends ChessPiece {
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		
-		
+			
 		return mat;
 	}
 }
